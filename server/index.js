9bc49b851f803a1694728e2a0dd57319cdedd583
Dallin Breen <dallinbreen624@gmail.com>
2022-02-24 12:11:07 -0700

    Added rollbar

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const path = require("path");
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '0587b4efbd614202b5c79ecb6fc58837',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express();
 
 app.get("/", (req, res) => {
